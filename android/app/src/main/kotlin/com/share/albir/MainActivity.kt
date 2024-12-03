658eca694677ec7a5709398e1f93c4e258f0f000
abdalmuneim <mo.m.eg21@gmail.com>
2024-12-03 06:26:18 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/share/albir/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.share.albir
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:nearpay_flutter_sdk/nearpay.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Nearpay nearpay;
+  final tokenKey = "info@share.net.sa";
+  final AuthenticationType authType = AuthenticationType.email;
+  final timeout = 60;
+
+  @override
+  void initState() {
+    super.initState();
+    initSDK();
+  }
+
+  initSDK() async {
+    nearpay = Nearpay(
+      authType: authType,
+      authValue: tokenKey,
+      env: Environments.sandbox,
+      locale: Locale.localeDefault,
+    );
+    //if you want to install the plugin immediatly after initializing then use the setup method (optional)
+    await nearpay.setup();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              ' ',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
